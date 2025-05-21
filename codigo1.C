48cf40e88437a8c06654a446b7417e8b6d81f054
Davi <164496370+Davi8002@users.noreply.github.com>
2025-05-21 16:09:02 -0300

    Update codigo1.C

--- codigo1.C
+++ codigo1.C
@@ -1,1 +1,40 @@
 
+#include <stdio.h>
+
+int main(void) {
+  
+  float numeroX, numeroY, resultado;
+  int operacao;
+  printf("Digite um número: \n");
+  scanf("%f", &numeroX);
+  printf("Digite:\n 1 para soma \n 2 para subtração\n 3 para multiplicação\n 4 para divisão \n 5 para média aritmética\n");
+  scanf("%d", &operacao);
+  printf("Digite outro número: \n");
+  scanf("%f", &numeroY);
+  switch(operacao){
+    case 1:
+    resultado = numeroX + numeroY;
+    break;
+    case 2:
+    resultado = numeroX - numeroY;
+    break;
+    case 3:
+    resultado = numeroX * numeroY;
+    break;
+    case 4:
+    resultado = numeroX / numeroY;
+    break;
+    case 5: 
+    resultado = (numeroX + numeroY)/2;
+    break;
+    default:
+    printf("Operação inválida.\n");
+    break;
+  }
+
+
+  
+  printf("Seu resultado é %1.1f", resultado);
+  
+  return 0;
+}
