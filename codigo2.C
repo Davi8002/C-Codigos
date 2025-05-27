2944374021f22869acf0c6b27ac9fa4d998dc134
Davi <164496370+Davi8002@users.noreply.github.com>
2025-05-26 22:49:09 -0300

    Update codigo2.C

--- codigo2.C
+++ codigo2.C
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <windows.h>
 
 int main(void) {
   int numeroRandom;
@@ -12,7 +13,7 @@ int main(void) {
   if(vidas <= 0){
     vidas = 1;
   }
-  system("clear"); // alterar dps pra cls do windows
+  system("cls"); // alterar dps pra cls do windows
 
   srand(time(NULL));
   numeroRandom = (rand() % 100);
@@ -27,6 +28,7 @@ int main(void) {
 
     if (numeroChute == numeroRandom) {
       printf("\n VOCE GANHOU HEHE!!");
+      Sleep(5000);
     } else if (numeroChute > numeroRandom) {
       printf("\nTENTE UM NUMERO MENOR\n\n");
       vidas--;
@@ -36,19 +38,20 @@ int main(void) {
     }
 
     if (vidas == 3 && numeroRandom != numeroChute){
-      if(numeroChute % 2 == 0){
+      if(numeroRandom % 2 == 0){
         printf("\nSEU NUMERO É PAR\n\n");
-      }else if(numeroRandom % 2 == 1 && numeroRandom != numeroChute){
+      }else if(numeroRandom % 2 == 1 ){
           printf("\nSEU NUMERO É IMPAR\n\n");
         }
     }//par impar
 
     
     if (vidas == 0) {
-      system("clear"); // alterar dps pra cls do windows
+      system("cls"); // alterar dps pra cls do windows
       printf("VOCE PERDEU!!!!\n");
       printf("Seu Pc vai desligar em 15 segundos visse!");
-      //system("shutdown /s /t 15");//alterar caso eu va usar no prompt porque da erro aqui
+      Sleep(5000);
+      system("shutdown /s /t 15");//alterar caso eu va usar no prompt porque da erro aqui
     }//quando as vidas acabam
   }
 }
