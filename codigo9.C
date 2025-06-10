89e4c5dc3bbc52099761eedcfe52c44c1da083a0
Davi <164496370+Davi8002@users.noreply.github.com>
2025-06-10 08:52:46 -0300

    Update codigo9.C

--- codigo9.C
+++ codigo9.C
@@ -1,1 +1,28 @@
+#include <stdio.h>
+
+int main(void) {
+  int h, v, backup;
+  int hi, vi;
+
+  printf("Digite medida Horizontal\n");
+  scanf("%d", &h);
+  printf("Digite medida Vertical\n");
+  scanf("%d", &v);
+  backup = h;
+  hi = h;
+  vi = v;
+    
+  while(v > 0){
+    v--;
+    printf("\n");
+    h = backup;
+    while (h > 0){
+    h--;
+    printf("-");  
+    }
+     }
+  backup = vi * hi;
+  printf("\n\n Seu retangulo %d por %d tem a Área de %d", hi, vi, backup);
+    return 0;
+}
 
