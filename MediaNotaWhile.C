81e7aedb129142148d44e4740edc9b4c3c9d26a2
Davi <164496370+Davi8002@users.noreply.github.com>
2025-06-18 10:48:00 -0300

    Create MediaNotaWhile.C

---
+++ MediaNotaWhile.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+  float totaltotal, totalnumero;
+  float numero = 0;
+  
+  while(numero >= 0){
+  printf("Digite a nota\n");
+  scanf("%f", &numero);
+  if(numero >= 0){
+  totaltotal += numero;
+  totalnumero++;
+  }}
+  
+  numero = totaltotal / totalnumero;
+  printf("Sua média é %1.2f", numero);
+  
+}
